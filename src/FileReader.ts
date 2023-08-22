31baf02645e6a265191f60f7919ca442efd1b9d7
myrear <mikuri.nakano@terass.com>
2023-08-22 20:13:50 +0900

    chore: explicit return type

--- src/FileReader.ts
+++ src/FileReader.ts
@@ -1,19 +1,19 @@
 import { readAs } from './readAs.ts'
 
 export class FileReader extends globalThis.FileReader {
-  readAsArrayBuffer(blob: Blob) {
+  readAsArrayBuffer(blob: Blob): Promise<ArrayBuffer> {
     return readAs(this, blob, 'ArrayBuffer')
   }
 
-  readAsBinaryString(blob: Blob) {
+  readAsBinaryString(blob: Blob): Promise<string> {
     return readAs(this, blob, 'BinaryString')
   }
 
-  readAsDataURL(blob: Blob) {
+  readAsDataURL(blob: Blob): Promise<string> {
     return readAs(this, blob, 'DataURL')
   }
 
-  readAsText(blob: Blob, encoding?: string) {
+  readAsText(blob: Blob, encoding?: string): Promise<string> {
     return readAs(this, blob, 'Text', encoding)
   }
 }
